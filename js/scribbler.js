f50430a034032659c386e9b0791ebe7783f594f8
Nevcan Uludas <nevcanuludas@gmail.com>
2023-08-02 13:53:19 +0400

    Fix content

--- js/scribbler.js
+++ js/scribbler.js
@@ -12,15 +12,17 @@ var getAll = function (selector, scope) {
 // setup typewriter effect in the terminal demo
 if (document.getElementsByClassName('demo').length > 0) {
   var i = 0;
-  var txt = `scribbler
-            [Entry mode; press Ctrl+D to save and quit; press Ctrl+C to quit without saving]
+  var txt = `Cleaning Sessions Guideline
 
-            ###todo for new year dinner party
+This document is prepared for cleaning operations in DDD. If you need more information you can reach Nevcan or Richard to get more.
 
-            - milk
-            - butter
-            - green onion
-            - lots and lots of kiwis 🥝`;
+# CHAPTERS
+
+- Roles
+- General Diving Rules
+- Equipment & Installation
+- How to vacuum
+- General Rules`;
   var speed = 60;
 
   function typeItOut () {
